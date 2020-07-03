c4b2a5cb10f777a12d02483e38f78b14a3218ffb
RutSzymon <rut.szymon@gmail.com>
2020-07-03 10:27:08 +0200

    212. Wiring Up the Google API Library

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "409492276387-01boqtdlt76ucum450e6ca307lo6qah8.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
